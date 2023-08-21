a68fe69269260c3de44a831902da5b925c8a2eee
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-21 12:13:05 -0500

    Return all posts

--- laravel-rest-api/6.blog-jwt/routes/api.php
+++ laravel-rest-api/6.blog-jwt/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,5 @@ Route::group([
   Route::post('refresh', [AuthController::class, 'refresh']);
   Route::get('profile', [AuthController::class, 'profile']);
 });
+
+Route::apiResource('posts', PostController::class);
