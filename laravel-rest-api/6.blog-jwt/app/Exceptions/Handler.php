956ac20203867aa07a4803c7894cdcd839e8e576
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-21 17:55:30 -0500

    Move exceptions messages to middleware authenticate

--- laravel-rest-api/6.blog-jwt/app/Exceptions/Handler.php
+++ laravel-rest-api/6.blog-jwt/app/Exceptions/Handler.php
@@ -2,10 +2,8 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class Handler extends ExceptionHandler
 {
@@ -28,22 +26,5 @@ class Handler extends ExceptionHandler
     $this->reportable(function (Throwable $e) {
       //
     });
-  }
-
-  public function render($request, Throwable $exception)
-  {
-    try {
-      JWTAuth::parseToken()->authenticate();
-    } catch (Exception $e) {
-      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-        return response()->json(['success' => false, 'message' => 'Token is Invalid'], 401);
-      } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-        return response()->json(['success' => false, 'message' => 'Token is Expired'], 401);
-      } {
-        return response()->json(['success' => false, 'message' => 'Authorization Token not found'], 401);
-      }
-    }
-
-    return parent::render($request, $exception);
   }
 }
--- laravel-rest-api/6.blog-jwt/app/Http/Middleware/Authenticate.php
+++ laravel-rest-api/6.blog-jwt/app/Http/Middleware/Authenticate.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Middleware;
 
+use Exception;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use JWTAuth;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
-    {
-        return $request->expectsJson() ? null : route('login');
+  /**
+   * Get the path the user should be redirected to when they are not authenticated.
+   */
+  protected function redirectTo(Request $request): ?string
+  {
+    try {
+      JWTAuth::parseToken()->authenticate();
+    } catch (Exception $e) {
+      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+        return response()->json(['success' => false, 'message' => 'Token is Invalid'], 401);
+      } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+        return response()->json(['success' => false, 'message' => 'Token is Expired'], 401);
+      } {
+        return response()->json(['success' => false, 'message' => 'Authorization Token not found'], 401);
+      }
     }
+    return $request->expectsJson() ? null : route('login');
+  }
 }
