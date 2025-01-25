861c891a08184ef8d9b8f59ec633f6ad89346734
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-24 22:18:37 -0500

    Configure api prefix

--- 13.tdd/bootstrap/app.php
+++ 13.tdd/bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        apiPrefix: '/api/v1',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
