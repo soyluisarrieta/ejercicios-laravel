4e958661ced43c2c96d127a670e347ef061a2d43
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-25 14:04:23 -0500

    Handle validation exception response

--- 13.tdd/bootstrap/app.php
+++ 13.tdd/bootstrap/app.php
@@ -3,17 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php',
         apiPrefix: '/api/v1',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $throwable) {
+            return jsonResponse(status: 422, message: $throwable->getMessage(), errors: $throwable->errors());
+        });
     })->create();
