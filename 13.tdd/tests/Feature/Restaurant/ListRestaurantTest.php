67d10ba105e2bbaa8a554625986f4f29e8569a9c
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-31 15:30:46 -0500

    Make an authenticated user must see their restaurants

---
+++ 13.tdd/tests/Feature/Restaurant/ListRestaurantTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Restaurant;
+use App\Models\User;
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListRestaurantTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $restaurants;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(UserSeeder::class);
+        $this->restaurants = Restaurant::factory()->count(10)->create([
+            'user_id' => 1
+        ]);
+    }
+
+    /**
+     * Un usuario autenticado puede obtener sus restaurantes
+     */
+    public function test_an_authenticated_user_must_see_their_restaurants(): void
+    {
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'GET', "{$this->apiBase}/restaurants");
+
+        # Esperando
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'message',
+            'data' => ['restaurants'],
+            'errors',
+            'status'
+        ]);
+        $response->assertJsonCount(10, 'data.restaurants');
+    }
+}
