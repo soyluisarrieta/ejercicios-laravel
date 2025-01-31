71e3b00f281834d5061f8fceb67daba7da159159
Luis Arrieta <luisarrieta796@gmail.com>
2025-01-31 13:00:22 -0500

    Add validations test to create restaurant

--- 13.tdd/tests/Feature/Restaurant/CreateRestaurantTest.php
+++ 13.tdd/tests/Feature/Restaurant/CreateRestaurantTest.php
@@ -50,4 +50,80 @@ class CreateRestaurantTest extends TestCase
             ...$data
         ]);
     }
+
+    /**
+     * El nombre del restaurante es requerido
+     */
+    public function test_restaurant_name_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => '',
+            'description' => 'New restaurant description',
+        ];
+
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'POST', "{$this->apiBase}/restaurants", $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * El nombre del restaurante es requerido
+     */
+    public function test_restaurant_name_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'ne',
+            'description' => 'New restaurant description',
+        ];
+
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'POST', "{$this->apiBase}/restaurants", $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * La descripción del restaurante es requerido
+     */
+    public function test_restaurant_description_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New restaurant',
+            'description' => '',
+        ];
+
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'POST', "{$this->apiBase}/restaurants", $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
+    }
+
+    /**
+     * La descripción del restaurante es requerido
+     */
+    public function test_restaurant_description_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New restaurant',
+            'description' => 'Ne',
+        ];
+
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'POST', "{$this->apiBase}/restaurants", $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
+    }
 }
