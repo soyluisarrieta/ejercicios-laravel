f28e90a79731e63ea2365491563e3aaf5f73cfd1
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-01 17:59:46 -0500

    Configure store plate request

--- 13.tdd/app/Http/Requests/StorePlateRequest.php
+++ 13.tdd/app/Http/Requests/StorePlateRequest.php
@@ -11,7 +11,7 @@ class StorePlateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
@@ -22,7 +22,9 @@ class StorePlateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
         ];
     }
 }
