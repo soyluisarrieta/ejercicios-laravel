3388456de87381027957b6b5e80492cc125f5274
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-01 15:20:44 -0500

    Make a user can see their paginated plates by restaurant test

--- 13.tdd/tests/Feature/Plate/ListPlateTest.php
+++ 13.tdd/tests/Feature/Plate/ListPlateTest.php
@@ -48,4 +48,36 @@ class ListPlateTest extends TestCase
         ]);
         $response->assertJsonCount(15, 'data.plates');
     }
+
+    /**
+     * Un usuario puede ver sus platos por restaurante paginados
+     */
+    public function test_a_user_can_see_their_paginated_plates_by_restaurant(): void
+    {
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'GET', "{$this->apiBase}/{$this->restaurant->id}/plates");
+
+        # Esperando
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data.plates');
+        $response->assertJsonStructure([
+            'message',
+            'errors',
+            'status',
+            'data' => [
+                'plates',
+                'total',
+                'current_page',
+                'per_page',
+                'total_pages',
+                'count'
+            ],
+        ]);
+
+        $response->assertJsonPath('data.total', 15);
+        $response->assertJsonPath('data.current_page', 1);
+        $response->assertJsonPath('data.per_page', 15);
+        $response->assertJsonPath('data.total_pages', 1);
+        $response->assertJsonPath('data.count', 15);
+    }
 }
