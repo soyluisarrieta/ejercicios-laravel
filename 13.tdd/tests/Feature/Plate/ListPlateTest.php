ec32136b14ea357851e2460c77e01d66a8d1ba91
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-01 14:28:56 -0500

    Make a user can see their plates test

---
+++ 13.tdd/tests/Feature/Plate/ListPlateTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Restaurant;
+use App\Models\User;
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListPlateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $plates;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(UserSeeder::class);
+        $this->plates = Restaurant::factory()->count(15)->create();
+    }
+
+    /**
+     * Un usuario puede ver sus platos
+     */
+    public function test_a_user_can_see_their_plates(): void
+    {
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'GET', "{$this->apiBase}/plates");
+
+        # Esperando
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'message',
+            'data' => ['plates'],
+            'errors',
+            'status'
+        ]);
+        $response->assertJsonCount(15, 'data.plates');
+    }
+}
