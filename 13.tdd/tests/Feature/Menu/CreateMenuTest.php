cba3e30c1ac8cac4ac5bdda4da497950b77c963e
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-02 16:15:35 -0500

    Make menu validation tests

--- 13.tdd/tests/Feature/Menu/CreateMenuTest.php
+++ 13.tdd/tests/Feature/Menu/CreateMenuTest.php
@@ -24,7 +24,7 @@ class CreateMenuTest extends TestCase
         $this->seed(UserSeeder::class);
         $this->user = User::factory()->create();
         $this->restaurant = Restaurant::factory()->create(['user_id' => $this->user->id]);
-        $this->plates = Plate::factory()->count(15)->create();
+        $this->plates = Plate::factory()->count(15)->create(['restaurant_id' => $this->restaurant->id]);
     }
 
     /**
@@ -32,8 +32,6 @@ class CreateMenuTest extends TestCase
      */
     public function test_a_user_can_create_a_menu(): void
     {
-        $this->withoutExceptionHandling();
-
         # Teniendo
         $data = [
             'name' => 'New menu',
@@ -85,5 +83,174 @@ class CreateMenuTest extends TestCase
                 'plate_id' => $plate->id,
             ]);
         }
+    }
+
+    /**
+     * El nombre del menu es requerido
+     */
+    public function test_menu_name_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => '',
+            'description' => 'New menu description',
+            'plate_ids' => $this->plates->pluck('id')
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * El nombre del menu debe tener al menos 3 caracteres
+     */
+    public function test_menu_name_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'Ne',
+            'description' => 'New menu description',
+            'plate_ids' => $this->plates->pluck('id')
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * La descripción del menu es requerido
+     */
+    public function test_menu_description_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New menu',
+            'description' => '',
+            'plate_ids' => $this->plates->pluck('id')
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
+    }
+
+    /**
+     * La descripción del menu debe tener al menos 3 caracteres
+     */
+    public function test_menu_description_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New menu',
+            'description' => 'Ne',
+            'plate_ids' => $this->plates->pluck('id')
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
+    }
+
+    /**
+     * Los platos del menu son requeridos
+     */
+    public function test_menu_plates_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New menu',
+            'description' => 'New menu description',
+            'plate_ids' => []
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['plate_ids']]);
+    }
+
+    /**
+     * Los platos del menu deben existir
+     */
+    public function test_menu_plates_must_exists(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New menu',
+            'description' => 'New menu description',
+            'plate_ids' => [100]
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['plate_ids.0']]);
+    }
+
+    /**
+     * El restaurante del menu debe pertener al usuario
+     */
+    public function test_menu_restaurant_must_belongs_to_user(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'New menu',
+            'description' => 'New menu description',
+            'plate_ids' => [1]
+        ];
+        $user = User::factory()->create();
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(403);
+    }
+
+    /**
+     * Los platos del menu deben pertener al usuario
+     */
+    public function test_menu_plates_must_belongs_to_user(): void
+    {
+        # Teniendo
+        $plate = Plate::factory()->create();
+        $data = [
+            'name' => 'New menu',
+            'description' => 'New menu description',
+            'plate_ids' => [$plate->id]
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus";
+        $response = $this->apiAs($this->user, 'POST', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['plate_ids.0']]);
     }
 }
