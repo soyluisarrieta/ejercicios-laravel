0e86a332dcb9974a7c80328b660b5cb9390bdd17
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-01 19:19:47 -0500

    Make update plate validation tests

--- 13.tdd/tests/Feature/Plate/UpdatePlateTest.php
+++ 13.tdd/tests/Feature/Plate/UpdatePlateTest.php
@@ -44,7 +44,6 @@ class UpdatePlateTest extends TestCase
         # Haciendo
         $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
         $response = $this->apiAs(User::find(1), 'PUT', $endpoint, $data);
-        dump($this->plate);
 
         # Esperando
         $response->assertStatus(200);
@@ -71,5 +70,110 @@ class UpdatePlateTest extends TestCase
             'description' => 'plate description',
             'price' => '$123',
         ]);
+    }
+
+    /**
+     * Un usuario no puede actualizar un platos de otro usuario
+     */
+    public function test_a_user_cannot_update_a_plate_of_another_users(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'NEW plate',
+            'description' => 'NEW plate description',
+            'price' => '$456'
+        ];
+        $user = User::factory()->create();
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
+        $response = $this->apiAs($user, 'PUT', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(403);
+    }
+
+    /**
+     * El nombre del plato es requerido
+     */
+    public function test_plate_name_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => '',
+            'description' => 'NEW plate description',
+            'price' => '$456'
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
+        $response = $this->apiAs(User::find(1), 'PUT', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * El nombre del plato debe tener al menos 3 caracteres
+     */
+    public function test_plate_name_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'NE',
+            'description' => 'NEW plate description',
+            'price' => '$456'
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
+        $response = $this->apiAs(User::find(1), 'PUT', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['name']]);
+    }
+
+    /**
+     * La descripción del plato es requerido
+     */
+    public function test_plate_description_field_must_be_required(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'NEW plate',
+            'description' => '',
+            'price' => '$456'
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
+        $response = $this->apiAs(User::find(1), 'PUT', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
+    }
+
+    /**
+     * La descripción del plato debe tener al menos 3 caracteres
+     */
+    public function test_plate_description_field_must_have_at_lease_3_characters(): void
+    {
+        # Teniendo
+        $data = [
+            'name' => 'NEW plate',
+            'description' => 'NE',
+            'price' => '$456'
+        ];
+
+        # Haciendo
+        $endpoint = "{$this->apiBase}/restaurants/{$this->restaurant->id}/plates/{$this->plate->id}";
+        $response = $this->apiAs(User::find(1), 'PUT', $endpoint, $data);
+
+        # Esperando
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['message', 'data', 'status', 'errors' => ['description']]);
     }
 }
