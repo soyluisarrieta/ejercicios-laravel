1a7855eeacc9b99b9284d63bfce16464ae03e0cc
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-06 10:56:58 -0500

    Make list menu test

---
+++ 13.tdd/tests/Feature/Menu/ListMenuTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Menu;
+use App\Models\Plate;
+use App\Models\Restaurant;
+use App\Models\User;
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+
+class ListMenuTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected User $user;
+    protected Restaurant $restaurant;
+    protected Collection $menus;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(UserSeeder::class);
+        $this->user = User::factory()->create();
+        $this->restaurant = Restaurant::factory()->create(['user_id' => $this->user->id]);
+        $plates = Plate::factory()->count(100)->create(['restaurant_id' => $this->restaurant->id,]);
+        $this->menu = Menu::factory()
+            ->count(150)
+            ->hasAttached($plates->random(15))
+            ->create(['restaurant_id' => $this->restaurant->id]);
+    }
+
+    /**
+     * Un usuario puede ver sus menus
+     */
+    public function test_a_user_can_see_their_menus(): void
+    {
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'GET', "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus");
+
+        # Esperando
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data.menus');
+        $response->assertJsonStructure([
+            'message',
+            'status',
+            'errors',
+            'data' => [
+                'menus' => [
+                    '*' => ['id', 'name', 'description', 'restaurant_id']
+                ]
+            ]
+        ]);
+        $response->assertJsonMissingPath('data.menus.0.plates');
+        $response->assertJsonPath('data.total', 150);
+        $response->assertJsonPath('data.current_page', 1);
+        $response->assertJsonPath('data.per_page', 15);
+        $response->assertJsonPath('data.count', 15);
+    }
+}
