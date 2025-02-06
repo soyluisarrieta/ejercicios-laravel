5ff810197b3110fa155e8f49de8d2dc51ccfeb0e
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-06 11:25:01 -0500

    Make list menu validation tests

--- 13.tdd/tests/Feature/Menu/ListMenuTest.php
+++ 13.tdd/tests/Feature/Menu/ListMenuTest.php
@@ -42,7 +42,6 @@ class ListMenuTest extends TestCase
 
         # Esperando
         $response->assertStatus(200);
-        $response->assertJsonCount(15, 'data.menus');
         $response->assertJsonStructure([
             'message',
             'status',
@@ -53,10 +52,38 @@ class ListMenuTest extends TestCase
                 ]
             ]
         ]);
-        $response->assertJsonMissingPath('data.menus.0.plates');
+    }
+
+    /**
+     * Un usuario puede ver sus menus paginados
+     */
+    public function test_a_user_can_see_their_paginated_menus(): void
+    {
+        # Haciendo
+        $response = $this->apiAs(User::find(1), 'GET', "{$this->apiBase}/restaurants/{$this->restaurant->id}/menus");
+
+        # Esperando
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data.menus');
+        $response->assertJsonStructure([
+            'data' => ['total', 'current_page', 'total_pages', 'per_page', 'count']
+        ]);
         $response->assertJsonPath('data.total', 150);
         $response->assertJsonPath('data.current_page', 1);
+        $response->assertJsonPath('data.total_pages', 10);
         $response->assertJsonPath('data.per_page', 15);
         $response->assertJsonPath('data.count', 15);
+    }
+
+    /**
+     * Un usuario no autenticado no puede ver los menus
+     */
+    public function test_an_unauthenticated_user_cannot_see_menus(): void
+    {
+        # Haciendo
+        $response = $this->getJson("{$this->apiBase}/restaurants/{$this->restaurant->id}/menus");
+
+        # Esperando
+        $response->assertStatus(401);
     }
 }
